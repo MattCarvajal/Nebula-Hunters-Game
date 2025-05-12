86be69c036233e31f48518031d851a2a19f0d63b
MattCarvajal <ma449997@ucf.edu>
2025-05-12 10:44:39 -0400

    Initial Commit

---
+++ Nebula Hunters GameTests/Nebula_Hunters_GameTests.swift
@@ -0,0 +1,17 @@
+//
+//  Nebula_Hunters_GameTests.swift
+//  Nebula Hunters GameTests
+//
+//  Created by Matt Carvajal on 5/12/25.
+//
+
+import Testing
+@testable import Nebula_Hunters_Game
+
+struct Nebula_Hunters_GameTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
